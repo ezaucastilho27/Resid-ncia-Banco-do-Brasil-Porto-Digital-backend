003468c3c7ede62acd5e689eda925f1849a5be4a
Mayara Almeida <jennim.almeida@gmail.com>
2025-12-02 05:35:50 -0300

    Configurado com Mongodb Atlas

--- kaiapo-audio-api-full/src/server.js
+++ kaiapo-audio-api-full/src/server.js
@@ -3,11 +3,12 @@ const app = require('./app');
 const connectDB = require('./config/db');
 
 const PORT = process.env.PORT || 3000;
+const MONGODB_URI = process.env.MONGODB_URI;
 
 connectDB()
   .then(() => {
     app.listen(PORT, () => {
-      console.log(`Servidor rodando em http://localhost:${PORT}`);
+      console.log(`Servidor rodando em ${MONGODB_URI}:${PORT}`);
     });
   })
   .catch(err => {
