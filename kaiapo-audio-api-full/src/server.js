40e839374f0833dcce2a43c6866959d053cbbca6
Mayara Almeida <jennim.almeida@gmail.com>
2025-12-02 08:01:11 -0300

    Read Me Atualizado

--- kaiapo-audio-api-full/src/server.js
+++ kaiapo-audio-api-full/src/server.js
@@ -3,7 +3,6 @@ const app = require('./app');
 const connectDB = require('./config/db');
 
 const PORT = process.env.PORT || 3000;
-const MONGODB_URI = process.env.MONGODB_URI;
 
 connectDB()
   .then(() => {
